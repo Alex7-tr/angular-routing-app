47e248d05ec4bb106c2cc1e437f4cffdba271a51
Alessandro <alessandro.cammarota@altran.it>
2020-12-15 18:02:07 +0100

    errori

---
+++ src/app/first/first.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute, ParamMap } from '@angular/router';
+import { Observable } from 'rxjs';
+import { switchMap } from 'rxjs/operators';
+
+@Component({
+  selector: 'app-first',
+  templateUrl: './first.component.html',
+  styleUrls: ['./first.component.scss']
+})
+export class FirstComponent implements OnInit {
+
+  constructor(
+    private route: ActivatedRoute,
+  ) {}
+
+  goToItems() {
+    this.router.navigate(['items'], { relativeTo: this.route });
+  }
+}
